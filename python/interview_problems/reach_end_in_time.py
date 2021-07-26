209213966a5a25b64f9dc6506253bc5571458444
Angelo Martínez <angelomartinezcal@gmail.com>
2021-07-26 18:08:23 -0500

    Create reach_end_in_time.py

---
+++ python/interview_problems/reach_end_in_time.py
@@ -0,0 +1,4 @@
+# Reach the end in time
+
+# A 2-D grid consisting of some blocked (represented as '#') and some unblocked (represented as '.') cells is given.
+# The starting position of a pointer is in the top-left corner of the grid.
