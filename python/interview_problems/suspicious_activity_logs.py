045fee63c68d7b433dd9fe67875bbde9ce0c602d
Angelo Martínez <angelomartinezcal@gmail.com>
2021-07-26 17:02:02 -0500

    Update suspicious_activity_logs.py

--- python/interview_problems/suspicious_activity_logs.py
+++ python/interview_problems/suspicious_activity_logs.py
@@ -23,3 +23,51 @@
 # Note: In the last log entry, user 12 was on both sides of the transaction. This counts as only 1 transaction for user 12.
 
 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'processLogs' function below.
+#
+# The function is expected to return a STRING_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY logs
+#  2. INTEGER threshold
+
+
+def processLogs(logs, threshold):
+    # Write your code here
+    
+    
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    logs_count = int(input().strip())
+
+    logs = []
+
+    for _ in range(logs_count):
+        logs_item = input()
+        logs.append(logs_item)
+
+    threshold = int(input().strip())
+
+    result = processLogs(logs, threshold)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
+
+
