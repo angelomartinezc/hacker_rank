bd9ff335fdb70cfe85048fd5c16e0558944eac61
Angelo Martínez <angelomartinezcal@gmail.com>
2021-07-26 22:23:53 -0500

    Update reach_end_in_time.py

--- python/interview_problems/reach_end_in_time.py
+++ python/interview_problems/reach_end_in_time.py
@@ -1,4 +1,14 @@
 # Reach the end in time
 
 # A 2-D grid consisting of some blocked (represented as '#') and some unblocked (represented as '.') cells is given.
-# The starting position of a pointer is in the top-left corner of the grid.
+# The starting position of a pointer is in the top-left corner of the grid. It is guaranteed that the starting position is in an unblocked cell. 
+# It is also guaranteed that the bottom-right cell is unblocked. Each cell of the grid is connected with its  right, left, top and bottom cell (if those cell exits).
+# It takes 1 second for a pointer to move from a cell to its adjacent cell. If the pointer can reach the bottom-right corner of the grid within k seconds, return the string 'Yes'
+# Otherwise, return the string 'No'.
+
+# Example
+# rows = 3
+# grid = ['..##', '#.##', '#...']
+# maxTime = 5
+
+
